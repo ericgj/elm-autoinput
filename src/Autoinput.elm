5d16cc095f9622f597989c32e1283a81e8440490
Eric Gjertsen <ericgj72@gmail.com>
2017-02-01 19:37:54 -0500

    add docs

--- src/Autoinput.elm
+++ src/Autoinput.elm
@@ -20,6 +20,53 @@ module Autoinput
     , menuItem
     )
 
+{-| 
+
+This library encapsulates an _autocomplete menu_: 
+an input element that, as you type, narrows a list of choices visible in a menu.
+
+Configuration and the list of choices are passed in to `update` and `view`,
+but do not form part of the state managed by this library. This ensures that
+your data does not go out of sync with the menu list -- and in fact you can
+change the list as the user input changes, enabling common cases such as
+remote querying.
+
+There is a basic usage [example][] to look at, and more are planned.
+
+# Prior art
+
+This library started as a fork of the [elm-autocomplete][] "accessible" example,
+and some of the view and configuration remains similar, although the underlying
+implementation is different.
+
+The configuration API is a work in progress, but owes a lot to
+[elm-sortable-table][] conventions.
+
+# View
+
+@docs view
+
+# Update
+
+@docs update
+
+# Configuration
+
+@docs config, defaultConfig, inputAttributes, inputStyle, menu, menuItem, customConfig 
+
+# State
+
+@docs State, state, empty, preselect, query, init
+
+
+[example]: https://github.com/ericgj/elm-autoinput/tree/master/examples/Demo.elm
+
+[elm-autocomplete]: https://github.com/thebritican/elm-autocomplete/tree/master/examples/src/AccessibleExample.elm
+
+[elm-sortable-table]: https://github.com/evancz/elm-sortable-table
+
+-}
+
 import String
 import List
 import Maybe
@@ -47,16 +94,27 @@ type Model id =
     , menu : Menu.Model
     }
 
+{-| 
+
+Note internal state used by this module has more information than the externally
+facing State. It distinguishes between pre-selection and selection, and retains
+the query string even after a selection is made.
+
+-}
 type InternalState id
   = Initial
   | Preselecting id
   | Querying String
   | Selecting String id
 
+
+{-| Input state. Note to get the current state, use `state model`.
+-}
 type State id
   = NoInput
   | Entered String
   | Selected id
+
 
 type Config item =
   Config
@@ -68,18 +126,27 @@ type Config item =
     , menuConfig : Menu.Config item 
     }
 
+{-| Construct an empty model (no input, hidden menu).
+-}
 empty : Model id
 empty =
   initInternal Initial
 
+
+{-| Construct a model with an initial selected value.
+-}
 preselect : id -> Model id
 preselect id =
   initInternal (Preselecting id)
 
+{-| Construct a model with an initial query value.
+-}
 query : String -> Model id
 query q =
   initInternal (Querying q)
 
+{-| Construct a model from given State.
+-}
 init : State id -> Model id
 init state =
   case state of
@@ -94,6 +161,8 @@ initInternal : InternalState id -> Model id
 initInternal state =
   Model { state = state, menu = Menu.empty }
 
+{-| Extract internal state of model to State.
+-}
 state : Model id -> State id
 state (Model model) =
   case model.state of
@@ -128,6 +197,40 @@ type Msg id
   | NoOp
 
 
+{-| 
+
+Handle input. Your application should nest Autoinput messages in the typical
+way using `Html.map`. Note that Autoinput Msg type takes an `id` parameter: the
+ID type of items in your list. 
+
+Here is an example:
+
+    -- view
+
+    div []
+        [ label [] [text "Select a thing"]
+        , Autoinput.view config things model |> Html.map UpdateAutoInput
+        ]
+
+    -- update
+    
+    type Msg 
+        -- ...
+        | UpdateAutoInput (Autoinput.Msg ThingId)
+        -- ...
+
+    update msg model =
+        case msg of
+            UpdateAutoInput automsg ->
+                Autoinput.update config things automsg model
+                
+
+Note that you pass two pieces of context in to `update`:
+
+  - The `Config`,  and
+  - The full list of items, as tuples of `(id, item)`.
+
+-}
 update :  Config item -> List (id, item) -> Msg id -> Model id -> Model id
 update (Config config) items msg (Model model) =
   let 
@@ -175,6 +278,16 @@ update (Config config) items msg (Model model) =
         (Model model)
 
 
+{-| 
+
+Render the input element and associated menu div.
+
+Note that the same as for `update`, you pass in two pieces of context:
+
+  - The `Config`,  and
+  - The full list of items, as tuples of `(id, item)`.
+
+-}
 view :  Config item -> List (id, item) -> Model id -> Html (Msg id)
 view (Config config) items (Model model) =
     let
@@ -308,15 +421,17 @@ inputValue toString items state =
         |> Maybe.withDefault ""
 
 
-{-| Note subtle difference between `queryValue` and `inputValue` when Selecting. 
-    
-    `inputValue` is the value of the input element, and should be the text of
-    the looked-up selected item. This is used in the `view`.
-    
-    `queryValue` is the last entered query text by the user, which determines
-    the menu choices. This is used in `update`.
+{-| 
 
-    In a nutshell, this is what makes state management tricky in autocomplete.
+Note subtle difference between `queryValue` and `inputValue` when Selecting. 
+    
+`inputValue` is the value of the input element, and should be the text of
+the looked-up selected item. This is used in the `view`.
+    
+`queryValue` is the last entered query text by the user, which determines
+the menu choices. This is used in `update`.
+
+In a nutshell, this is what makes state management tricky in autocomplete.
 
 -}
 queryValue : (item -> String) -> List (id,item) -> InternalState id -> String
@@ -354,6 +469,51 @@ menuItems search howMany items state =
 
 -- CONFIG
 
+{-| Create the `config` to pass into `update` and `view`. Everything we need in 
+order to filter and display the menu in response to user input. 
+
+For example, say we want to build a menu of `Person`s, searching either their
+name or their email address for any match of the entered text, but only display
+their names in the menu. We might have a `Config` like this:
+
+    import Autoinput
+
+    config : Autoinput.Config Person
+    config =
+        Autoinput.config
+            { howMany = 5
+            , search = searchNameAndEmail
+            , toString = .name
+            , menuId = "person-menu"
+            , menuItemStyle = menuItem
+            }
+
+    searchNameAndEmail : String -> Person -> Bool
+    searchNameAndEmail query person =
+        [person.name, person.email]
+            |> List.map String.toLower
+            |> List.any (String.contains (String.toLower query)) 
+
+    menuItem : Bool -> List (String, String)
+    menuItem selected =
+        if selected then
+            [ ("background-color", "light-yellow") ]
+        else
+            [ ]
+
+You provide the following information:
+
+  - `howMany` &mdash; maximum number of matched items to display in menu
+  - `search` &mdash; search function `String -> item -> Bool`
+  - `toString` &mdash; how your items should be displayed
+  - `menuId` &mdash; the DOM id of the "menu" element
+  - `menuItemStyle` &mdash; how a menu item should be styled depending on if it
+     is selected or not.
+
+More fine-grained configuration is possible through `defaultConfig` and
+`customConfig`.
+
+-}
 config :
     { howMany : Int
     , search : (String -> item -> Bool)
@@ -379,6 +539,8 @@ config { howMany, search, toString, menuId, menuItemStyle } =
             , menuConfig = Menu.defaultConfig |> Menu.menuItem menuItem_
             }
 
+{-| TODO
+-}
 defaultConfig :
     { howMany : Int
     , search : (String -> item -> Bool)
@@ -396,6 +558,8 @@ defaultConfig { howMany, search, toString, menuId } =
         , menuConfig = Menu.defaultConfig
         }
 
+{-| TODO
+-}
 inputAttributes : List (Html.Attribute Never) -> Config item -> Config item
 inputAttributes attrs (Config c) =
     let
@@ -403,6 +567,8 @@ inputAttributes attrs (Config c) =
     in
       Config { c | input = setAttrs c.input }
 
+{-| TODO
+-}
 inputStyle : List (String,String) -> Config item -> Config item
 inputStyle styles (Config c) =
     let
@@ -410,19 +576,27 @@ inputStyle styles (Config c) =
     in
       Config { c | input = setStyle c.input }
 
+{-| TODO
+-}
 menu : Menu.Config item -> Config item -> Config item
 menu m (Config c) =
     Config { c | menuConfig = m }
 
 
+{-| TODO
+-}
 menuItem : ( Bool -> item -> HtmlDetails ) -> Config item -> Config item
 menuItem fn (Config c) =
     Config { c | menuConfig = Menu.menuItem fn c.menuConfig }
 
+{-| TODO
+-}
 menuStyle : List (String,String) -> Config item -> Config item
 menuStyle styles (Config c) =
     Config { c | menuConfig = Menu.menuStyle styles c.menuConfig }
 
+{-| TODO
+-}
 menuAttributes : List (Html.Attribute Never) -> Config item -> Config item
 menuAttributes attrs (Config c) =
     Config { c | menuConfig = Menu.menuAttributes attrs c.menuConfig }
@@ -433,6 +607,8 @@ defaultInput =
   { attributes = [], style = [] }
 
 
+{-| TODO
+-}
 customConfig : 
     { howMany : Int
     , search : (String -> item -> Bool)
